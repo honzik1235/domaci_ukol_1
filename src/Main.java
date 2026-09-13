aa81c72bede8bf6acea92b85fe87c0931139c497
Honzik1235 <honzakadlcek@email.cz>
2026-09-13 17:29:52 +0200

    zaklad

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("O prázdninách jsem z velké části odpočíval, ale taky jsem pracoval a nebo jsem zkoušel nové věci." +
+                "Taky jsem třeba chodil po hhorách a nebo dělal to, na co mi přes rok nezbyl čas.");
+    }
+}
