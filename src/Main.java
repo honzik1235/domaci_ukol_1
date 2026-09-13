dfcfb0365eda5d450508a1867ee5a4b7eaf52cb8
Honzik1235 <honzakadlcek@email.cz>
2026-09-13 17:31:29 +0200

    zaklad

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("O prázdninách jsem z velké části odpočíval, ale taky jsem pracoval a nebo jsem zkoušel nové věci." +
                 "Taky jsem třeba chodil po hhorách a nebo dělal to, na co mi přes rok nezbyl čas.");
+
+
+        System.out.println("Jan Kadlček, HLuk, 730456456, kadlcek_jan@oauh.cz");
+
+
     }
 }
